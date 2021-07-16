52a60d483205e76372a8e9dfb4caf66554cf83c0
LuiArm <Luisarmendariz24@icloud.com>
2021-07-15 23:13:10 -0600

    daily algo

--- Algo.js
+++ Algo.js
@@ -42,4 +42,22 @@ function countBy(x, n) {
     return getAccumValue === value;
   }
  
- 
+
+// When writing on Twitter, he is known for almost always capitalizing every word. 
+// For simplicity, you'll have to capitalize each word, 
+// check out how contractions are expected to be in the example below.
+// Your task is to convert strings to how they would be written by Jaden Smith.
+//  The strings are actual quotes from Jaden Smith, 
+//  but they are not capitalized in the same way he originally typed them.
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+String.prototype.toJadenCase = function () {
+
+  function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  return this.split(' ').map(capitalizeFirstLetter).join(' ');
+};
